48af4c3a956df867ed7bb317f61e3da6c32b28ca
Nicolás Guzmán <n3kguzman@gmail.com>
2024-05-04 17:54:31 -0400

    creacion tablas

--- models.py
+++ models.py
@@ -1,5 +1,4 @@
 from flask_sqlalchemy import SQLAlchemy
-
 db = SQLAlchemy()
 
 
@@ -14,3 +13,35 @@ class User(db.Model):
     birthday = db.Column(db.String(120), nullable=False)
     active = db.Column(db.Boolean, default=True)
     admin = db.Column(db.Boolean, default=False)
+
+
+class Pipo(db.Model):
+    __tablename__ = 'pipos'
+    id = db.Column(db.Integer, primary_key=True)
+    pipo_name = db.Column(db.String(120))
+    address = db.Column(db.String(120))
+    latitude = db.Column(db.Float)
+    longitude = db.Column(db.Float)
+    active = db.Column(db.Boolean, default=True)
+    free = db.Column(db.Boolean)
+    disabled = db.Column(db.Boolean)
+    toiletpaper = db.Column(db.Boolean)
+    babychanger = db.Column(db.Boolean)
+
+
+class Comment(db.Model):
+    __tablename__ = 'comments'
+    id = db.Column(db.Integer, primary_key=True)
+    comment = db.Column(db.String(120))
+    date = db.Column(db.DateTime())
+    pipo_id = db.Column(db.Integer, db.ForeignKey('pipos.id'), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+
+
+class Rating(db.Model):
+    __tablename__ = 'ratings'
+    id = db.Column(db.Integer, primary_key=True)
+    stars = db.Column(db.Integer)
+    pipo_id = db.Column(db.Integer, db.ForeignKey('pipos.id'), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+
